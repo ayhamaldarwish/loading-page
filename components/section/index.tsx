e14e721f1137775e302fdf56e2a1bbb8c06d6e37
Ayham Al Darwish <126675498+ayhamaldarwish@users.noreply.github.com>
2025-05-18 11:52:58 +0300

    Update index.tsx

--- components/section/index.tsx
+++ components/section/index.tsx
@@ -1,9 +1,9 @@
 `use client`;
-/* import { Contact } from "./Contact.jsx"; */
+import { Contact } from "./Contact.jsx";
 import { Hero } from "./Hero";
 import { Services } from "./Services";
 import { AboutUs } from "./AboutUs";
 import { Header as Navbar } from "../Header";
 import { Footer } from "../Footer";
 
-export { Hero, AboutUs, Services, Navbar, Footer };
+export { Hero, AboutUs, Services, Contact, Navbar, Footer };
